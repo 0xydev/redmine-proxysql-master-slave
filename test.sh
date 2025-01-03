22a8d5357ab7fbc1b190436867d94fbedd1757c2
0xydev <furkanadkt@gmail.com>
2025-01-03 16:20:33 +0300

    test scripti güncellendi

--- test.sh
+++ test.sh
@@ -1,44 +1,19 @@
 #!/bin/bash
 
-# Script to Test ProxySQL Routing
+while true; do
+    echo -e "\n=== $(date '+%Y-%m-%d %H:%M:%S') ==="
+    echo -e "\nSorgu Dağılımı ve Yönlendirme İstatistikleri:"
+    docker compose exec proxysql mysql -h 127.0.0.1 -P 6032 -uadmin -padmin -e "
+    SELECT hostgroup, digest_text, count_star as hit_count, sum_time as total_time_ms 
+    FROM stats_mysql_query_digest 
+    WHERE digest_text IS NOT NULL 
+    ORDER BY hostgroup, count_star DESC;"
 
-# Variables
-PROXYSQL_HOST="proxysql"
-PROXYSQL_PORT=6033
-PROXYSQL_USER="redmine"
-PROXYSQL_PASSWORD="redmine_password"
-TEST_DB="redmine"
-TEST_TABLE="issues"
-
-# Function to execute MySQL commands via ProxySQL
-execute_mysql() {
-    local query="$1"
-    docker compose exec proxysql mysql -h "$PROXYSQL_HOST" -P "$PROXYSQL_PORT" -u "$PROXYSQL_USER" -p"$PROXYSQL_PASSWORD" -e "$query"
-}
-
-echo "=== ProxySQL Routing Test Started ==="
-
-for i in {1..3}
-do
-
-
-    # 2. Retrieve Issues to Verify Routing
-    echo "Retrieving issues..."
-    execute_mysql "
-    USE $TEST_DB;
-    SELECT * FROM users;
-    "
-
-    # 3. Check ProxySQL Statistics
-    echo "Fetching ProxySQL statistics..."
+    echo -e "\nSunucu Bağlantı Havuzu Durumu:"
     docker compose exec proxysql mysql -h 127.0.0.1 -P 6032 -uadmin -padmin -e "
     SELECT hostgroup, srv_host, status, ConnUsed, ConnFree, ConnOK, ConnERR 
-    FROM stats_mysql_connection_pool WHERE hostgroup IN (10, 20);
-    
-    SELECT rule_id, destination_hostgroup, hits 
-    FROM stats_mysql_query_rules ORDER BY rule_id;
-    "
+    FROM stats_mysql_connection_pool 
+    WHERE hostgroup IN (10, 20);"
 
+    sleep 5
 done
-
-echo "=== ProxySQL Routing Test Completed ==="
