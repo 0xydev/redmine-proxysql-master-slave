0b4a395578b2e8c29566a38f06865c8c67027fcc
0xydev <furkanadkt@gmail.com>
2025-01-07 18:20:19 +0300

    Redis session store'un çalıştığı gözlemlendi.

--- common/config/initializers/redis_session_store.rb
+++ common/config/initializers/redis_session_store.rb
@@ -2,9 +2,9 @@ require 'redis-rails'
 
 Rails.application.config.session_store :redis_store,
   servers: {
-    host: Rails.configuration.redis_host,
-    port: Rails.configuration.redis_port,
-    db: Rails.configuration.redis_db_name,
+    host: 'redis',
+    port: 6379,
+    db: 0,
     namespace: "redmine:session"
   },
   expire_after: 8.hours,
